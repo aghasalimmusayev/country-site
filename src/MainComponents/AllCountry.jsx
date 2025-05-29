68c3fb1b6a037ab36c2ae9c71730b532b0910e49
Agasalim <aga77ams@gmail.com>
2025-05-29 19:09:16 +0400

    4-th commit Show more button elave olundu

--- src/MainComponents/AllCountry.jsx
+++ src/MainComponents/AllCountry.jsx
@@ -1,27 +1,42 @@
 import { NavLink } from "react-router-dom"
 import "../CSS/allCountry.css"
+import { useState } from "react";
 
 function AllCountry({ countries }) {
+
+    const [count, setCount] = useState(6)
+    function showMore() {
+        setCount(count + 6)
+    }
+
     return (
-        <div className="countries">
-            {countries.map(country => (
-                <NavLink key={country.numericCode} to={`/details/${country.alpha3Code}`}>
-                    <div className="country-card" >
-                        <div>
-                            <img src={country.flags?.png} alt="flag" />
-                        </div>
-                        <h3>{country.name} - {country.alpha3Code}</h3>
-                        <p className="region">Region: {country.region}</p>
-                        <p className="capital">Capital: {country.capital}</p>
-                        <div className="pop-area">
-                            <p className="area">Area: {country.area}</p>
-                            <p className="population">Population: {country.population}</p>
-                        </div>
-                        <p className="currency">Currency: {country.currencies?.[0]?.code || "N/A"}</p>
-                    </div>
-                </NavLink >
-            ))}
-        </div>
+        <>
+            <div className="countries">
+                {countries
+                    .slice(0, count)
+                    .map(country => (
+                        <NavLink key={country.numericCode} to={`/details/${country.alpha3Code}`}>
+                            <div className="country-card" >
+                                <div>
+                                    <img src={country.flags?.png} alt="flag" />
+                                </div>
+                                <h3>{country.name} - {country.alpha3Code}</h3>
+                                <p className="region">Region: {country.region}</p>
+                                <p className="capital">Capital: {country.capital}</p>
+                                <div className="pop-area">
+                                    <p className="area">Area: {country.area}</p>
+                                    <p className="population">Population: {country.population}</p>
+                                </div>
+                                <p className="currency">Currency: {country.currencies?.[0]?.code || "N/A"}</p>
+                            </div>
+                        </NavLink >
+                    ))}
+            </div>
+            {count < countries.length &&
+                <div className="more_btn_box">
+                    <button className="more_btn" onClick={showMore}>Load More</button>
+                </div>}
+        </>
     );
 }
 
--- src/MainComponents/Regions.jsx
+++ src/MainComponents/Regions.jsx
@@ -5,6 +5,11 @@ function Regions({ countries }) {
 
     const { regionName } = useParams()
     const [regionCoutries, setRegionCoutries] = useState([]);
+    const [count, setCount] = useState(6)
+
+    function showMore() {
+        setCount(count + 6)
+    }
 
     useEffect(() => {
         const filtered = countries.filter(olkeler => olkeler.region === regionName)
@@ -12,25 +17,33 @@ function Regions({ countries }) {
     }, [countries, regionName])
 
     return (
-        <div className="countries">
-            {regionCoutries.map(country => (
-                <NavLink key={country.numericCode} to={`/details/${country.alpha3Code}`}>
-                    <div className="country-card" >
-                        <div>
-                            <img src={country.flags?.png} alt="flag" />
+        <>
+            <div className="countries">
+                {regionCoutries
+                .slice(0, count)
+                .map(country => (
+                    <NavLink key={country.numericCode} to={`/details/${country.alpha3Code}`}>
+                        <div className="country-card" >
+                            <div>
+                                <img src={country.flags?.png} alt="flag" />
+                            </div>
+                            <h3>{country.name} - {country.alpha3Code}</h3>
+                            <p className="region">Region: {country.region}</p>
+                            <p className="capital">Capital: {country.capital}</p>
+                            <div className="pop-area">
+                                <p className="area">Area: {country.area}</p>
+                                <p className="population">Population: {country.population}</p>
+                            </div>
+                            <p className="currency">Currency: {country.currencies?.[0]?.code || "N/A"}</p>
                         </div>
-                        <h3>{country.name} - {country.alpha3Code}</h3>
-                        <p className="region">Region: {country.region}</p>
-                        <p className="capital">Capital: {country.capital}</p>
-                        <div className="pop-area">
-                            <p className="area">Area: {country.area}</p>
-                            <p className="population">Population: {country.population}</p>
-                        </div>
-                        <p className="currency">Currency: {country.currencies?.[0]?.code || "N/A"}</p>
-                    </div>
-                </NavLink >
-            ))}
-        </div>
+                    </NavLink >
+                ))}
+            </div>
+            {count < countries.length &&
+                <div className="more_btn_box">
+                    <button className="more_btn" onClick={showMore}>Load More</button>
+                </div>}
+        </>
     )
 }
 
